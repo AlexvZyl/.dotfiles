ecf11ed9b2d9923350aedbeabfddfa3533ac0eb6
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-07-28 01:35:13 +0200

    Expand install script

--- install.sh
+++ install.sh
@@ -1,22 +1,48 @@
-# Use yay to get pacac.  yay is installed by default on EndeavourOS.
+#!/bin/bash
+
+# Ensure submodules are updated.
+git submodule update
+
+# Use yay to get pamac (installed by default on EndeavourOS).
 # yay -S libpamac-aur pamac-all # The full version is not currently building.
 yay -S libpamac-aur pamac-aur
 
 # Browser.
-sudo pamac install brave-bin
-sudo pamac remove firefox
+sudo pamac install --no-confirm brave-bin 
+sudo pamac remove firefox --no-confirm
 
-# Install Nerd Fonts.
-sudo pamac install nerd-fonts-complete
+# Fonts.  This is very large, maybe use smaller package.
+sudo pamac install nerd-fonts-complete --no-confirm
 
-# Icons.  Has to be added in tweaks.
-sudo pamac install papirus-icon-theme
+# Icons.
+sudo pamac install papirus-icon-theme --no-confirm
 
-# Setup alacritty.
-sudo pamac install alacritty
+# Terminal.
+sudo pamac install alacritty --no-confirm
 
-# Setup fish.
-sudo pamac install fish
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install IlanCosman/tide@v5
-tide configure
+# Coding stuff.
+sudo pamac install neovim --no-confirm
+sudo pamac install neovide --no-confirm
+# Install plugin for nvim.
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo pamac install github-desktop --no-confirm
+sudo pamac install code --no-confirm
+
+# Communication.
+sudo pamac install whatsapp-nativefier discord signal-desktop --no-confirm
+
+# Programming languages.
+sudo pamac install julia-bin --no-confirm
+
+# Setup fish (shell).
+sudo pamac install fish --no-confirm
+fish <<'END_FISH'
+	curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+	fisher install IlanCosman/tide@v5t
+END_FISH
+
+# Copy data over.
+# For now I do not want the home directory to be a git repo, so copy the relevant folders over.
+rsync -a -r  .config/ ~/.config/
+rsync -a -r  .local/ ~/.local/

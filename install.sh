5d0e77de335caabcff2448a294347e560440b926
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-07-28 00:27:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
+# Use yay to get pacac.  yay is installed by default on EndeavourOS.
+# yay -S libpamac-aur pamac-all # The full version is not currently building.
+yay -S libpamac-aur pamac-aur
+
+# Browser.
+sudo pamac install brave-bin
+sudo pamac remove firefox
+
 # Install Nerd Fonts.
 sudo pamac install nerd-fonts-complete
+
+# Icons.  Has to be added in tweaks.
+sudo pamac install papirus-icon-theme
 
 # Setup alacritty.
 sudo pamac install alacritty

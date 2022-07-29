8ed80b71bdbe01052e352e92d5e98caeb161bf4e
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-07-29 09:51:49 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set -g tide_git_icon 
 set -g tide_status_icon_failure 
 
 # Add alias for dotfile tracking with git.
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # Add neofetch on startup.
 neofetch

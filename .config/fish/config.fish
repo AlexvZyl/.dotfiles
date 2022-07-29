50c52792650127771970ce4771e340ffb4aa7a99
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-07-29 09:50:58 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,6 @@ set -g tide_left_prompt_items os context pwd git newline character
 set -g tide_git_icon 
 set -g tide_status_icon_failure 
 
-
 # Add alias for dotfile tracking with git.
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 

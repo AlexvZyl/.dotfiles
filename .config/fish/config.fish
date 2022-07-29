b58ceb113712912e03c9f6937a3e22167906d0bd
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-07-29 09:50:47 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,15 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# Setup fish.
+# Disable fish greeting.
 set fish_greeting "" 
 
-#-------------#
-# Setup tide. #
-#-------------#
-
-# Customize the right hand side.
+# Customize tide.
 set -g tide_right_prompt_items status cmd_duration jobs time
 set -g tide_left_prompt_items os context pwd git newline character 
-
-# Icons. 
 set -g tide_git_icon 
 set -g tide_status_icon_failure 
 
+
+# Add alias for dotfile tracking with git.
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
+# Add neofetch on startup.
 neofetch

830e196df9cd7da667012f37ead0b482eb8c2624
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-08-01 00:12:15 +0200

    Picom shadows and i3 vsync

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set fish_greeting "" 
 
 # Customize tide.
-set -g tide_right_prompt_items status cmd_duration jobs time
+# set -g tide_right_prompt_items status cmd_duration jobs time
 set -g tide_left_prompt_items os context pwd git newline character 
 set -g tide_git_icon 
 set -g tide_status_icon_failure 

c9162add533541c1ae2f197f54c3962d5f2474a0
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-08-22 22:13:07 +0200

    Remove unneccesary files and add dunst to install

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -37,7 +37,7 @@ sudo pamac install cmatrix bonsai.sh-git pipes.sh lolcat shell-color-scripts --n
 sudo pamac install nerd-fonts-complete --no-confirm
 
 # Utilities.
-sudo pamac install btop nvtop lazygit flameshot brightnessctl pfetch bottom --no-confirm
+sudo pamac install btop nvtop lazygit flameshot brightnessctl pfetch bottom dunst --no-confirm
 
 # Icons.
 sudo pamac install papirus-icon-theme --no-confirm

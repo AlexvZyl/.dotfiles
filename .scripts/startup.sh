967f6c1b54ba23d457e14b8bade542b5b22cb42e
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-09-02 00:30:02 +0200

    Minor

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -3,12 +3,12 @@
 # Setup the arandr monitor layout.
 ~/.screenlayout/default_triple_monitor.sh 
 
-# Launch polybar.
-~/.config/polybar/launch.sh
-
 # Get rid of that screen tearing.
 # Unsure if this will make startup slower?...
 nvidia-force-comp-pipeline
 
 # Start compositor.
 picom -b --experimental-backend
+
+# Launch polybar.
+~/.config/polybar/launch.sh

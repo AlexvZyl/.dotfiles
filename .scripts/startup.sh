c009d6b8c5da74356bcf02457633006b48b9e437
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-09-02 14:34:18 +0200

    Add double monitor xrandr script

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 # Setup the arandr monitor layout.
-~/.screenlayout/default_triple_monitor.sh 
+# ~/.screenlayout/default_triple_monitor.sh 
+~/.screenlayout/default_double_monitor.sh
 
 # Get rid of that screen tearing.
 # Unsure if this will make startup slower?...

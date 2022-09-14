5a13a98d566cd1945ea532dc7c5fedcbc44ad863
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-09-14 09:40:10 +0200

    Polybar: Add themes

---
+++ .config/polybar.personal/launch.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# if type "xrandr" > /dev/null; then
+    # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        # MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+    # done
+# else
+    # polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+# fi
+
+# second polybar at bottom
+# if type "xrandr" > /dev/null; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+#   done
+# else
+# polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+# fi
+#;;
+
+polybar --quiet --reload mainbar-i3 -c ~/.config/polybar/config.ini &
+
+

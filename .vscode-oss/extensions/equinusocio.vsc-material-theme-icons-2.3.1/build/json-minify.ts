338e2c46427e12075fac89152c72402acc90d01a
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-09-29 10:52:57 +0200

    VSCode: Chill out icons

--- .vscode-oss/extensions/equinusocio.vsc-material-theme-icons-2.3.1/build/json-minify.ts
+++
@@ -1,6 +0,0 @@
-import * as execa from 'execa';
-
-import {PATHS} from './helpers/paths';
-
-export default (): execa.ExecaChildProcess =>
-  execa(`json-minify ${PATHS.tmpPathIcons} > ${PATHS.pathIcons} && rimraf ${PATHS.tmpPathIcons}`, { shell: true });

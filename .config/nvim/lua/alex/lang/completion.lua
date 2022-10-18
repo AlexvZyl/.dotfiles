21de95ca8f37d98f5ecd019826a6b47bc2f52580
Alex-vZyl <alexandervanzyl@protonmail.com>
2022-10-18 19:53:05 +0200

    Neovim: Update nvim-cmp

--- .config/nvim/lua/alex/lang/completion.lua
+++ .config/nvim/lua/alex/lang/completion.lua
@@ -142,7 +142,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Get capabilities.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Setup sources.
 require('lspconfig')['sumneko_lua'].setup {

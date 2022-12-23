4bfa149e348f47554ce6db9337ec9790b14bcf2c
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-12-23 23:10:33 +0200

    Polybar: Fix CPU temp

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -11,5 +11,11 @@ nvidia-force-comp-pipeline
 # Start compositor.
 picom -b 
 
+# Create env variable for polybar CPU temp.
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: temp1_input" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
 # Launch polybar.
 ~/.config/polybar.personal/launch.sh

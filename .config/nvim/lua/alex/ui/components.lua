1a77851b25111d382d47240bdd4671f465bce0ce
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-12-26 15:50:56 +0200

    Neovim: Calculate dashboard padding on startup

--- .config/nvim/lua/alex/ui/components.lua
+++ .config/nvim/lua/alex/ui/components.lua
@@ -89,9 +89,24 @@ db.custom_header = {
     '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
 }
 
-db.header_pad = 9
-db.center_pad = 9
-db.footer_pad = 9
+-- Default sizes.
+local header_height = 6
+local center_height = 9
+local footer_height = 1
+
+-- Extra padding.
+local header_extra_padding = 1
+local center_extra_padding = 0
+local footer_extra_padding = 1
+
+-- Get window height in rows.
+local win_height = vim.fn.winheight('%')
+local padding = (win_height - header_height - center_height - footer_height) / 4
+
+-- Calculate and set padding for each section.
+db.header_pad = padding - header_extra_padding
+db.center_pad = padding - center_extra_padding
+db.footer_pad = padding - footer_extra_padding
 
 ---------------
 -- Scrollbar --

630382c530432b0a7ed0727307599532b1e8f43f
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-12-29 19:41:39 +0200

    Picom + Polybar: Read mode state file and watching

--- .config/picom/scripts/toggle-picom-inactive-opacity.sh
+++ .config/picom/scripts/toggle-picom-inactive-opacity.sh
@@ -2,6 +2,7 @@
 NO_DIM="inactive-opacity = 1.00;"
 DIM="inactive-opacity = 0.85;"
 FILENAME="/home/alex/.config/picom/picom.conf"
+STATE_FILE="/home/alex/.config/picom/read-mode-state"
 
 # Check dim state.
 if grep -Fxq "$DIM" $FILENAME
@@ -9,8 +10,11 @@ if grep -Fxq "$DIM" $FILENAME
 # Currently dim.
 then
     sed -i "s/$DIM/$NO_DIM/g" $FILENAME
+    echo ' ' >> $STATE_FILE
 
 # Currently no dim.
 else
     sed -i "s/$NO_DIM/$DIM/g" $FILENAME
+    echo ' ' >> $STATE_FILE
+
 fi

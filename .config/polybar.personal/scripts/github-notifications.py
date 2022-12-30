a8bf293da26e3d46059b29c68a6ff2b08a805ab1
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-12-30 11:33:43 +0200

    Polybar: Minor formatting

--- .config/polybar.personal/scripts/github-notifications.py
+++ .config/polybar.personal/scripts/github-notifications.py
@@ -2,18 +2,14 @@
 import subprocess
 import json
 
-# Command string.
-command = "gh"
-arg0 = "api"
-arg1 = "-H" 
-arg2 = "Accept: application/vnd.github+json"
-arg3 = "/notifications"
+command = [ "gh", "api", "-H", "Accept: application/vnd.github+json", "/notifications" ]
 
 # Try to connect to GitHub server and get notiication count.
 try:
-    result = subprocess.run([command, arg0, arg1, arg2, arg3], stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')
     json_object = json.loads(result)
-    print(len(json_object))
+    print(len(json_object), ' ')
+
 # Could not connect.
 except(json.JSONDecodeError):
     print(" ")

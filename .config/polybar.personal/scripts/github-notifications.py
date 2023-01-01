2cd506e64033e197e9956aa14f722a0ed877c74f
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-01 22:30:06 +0200

    Polybar:  Minor formatting and colors

--- .config/polybar.personal/scripts/github-notifications.py
+++ .config/polybar.personal/scripts/github-notifications.py
@@ -8,7 +8,7 @@ command = [ "gh", "api", "-H", "Accept: application/vnd.github+json", "/notifica
 try:
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')
     json_object = json.loads(result)
-    print(len(json_object), ' ')
+    print(len(json_object))
 
 # Could not connect.
 except(json.JSONDecodeError):

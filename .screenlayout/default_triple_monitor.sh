30af462c76a071aabe0a64c557f37059441b7a3f
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-14 21:43:06 +0200

    Screenlayout: Update triple monitor script

--- .screenlayout/default_triple_monitor.sh
+++ .screenlayout/default_triple_monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off
+xrandr --output DP-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --mode 1920x1080 --pos 3840x0 --rotate normal

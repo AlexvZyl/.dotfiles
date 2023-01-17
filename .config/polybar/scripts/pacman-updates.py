cd0462b520c18d81c8b643612669cfcdb1734601
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-17 23:27:40 +0200

    Polybar: Use yay instead of pacman

--- .config/polybar/scripts/pacman-updates.py
+++ .config/polybar/scripts/pacman-updates.py
@@ -3,11 +3,11 @@ import subprocess
 from collections import Counter
 
 # Sync.
-command1 = ["sudo", "yay", "-Syy"]
+command1 = [ "yay", "-Syy" ]
 subprocess.run(command1, stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')
 
 # Get updates.
-command2 = ["yay", "-Qu"]
+command2 = [ "yay", "-Qu" ]
 updates = subprocess.run(command2, stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')
 
 # Count packages.

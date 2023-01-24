dd37bff94efafb23ba0b0666a28771d8280a2e7a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-24 11:27:53 +0200

    Neovim: More porting to nordic.nvim

---
+++ .config/nvim/lua/alex/theme/nordic.lua
@@ -0,0 +1,21 @@
+-- Setup.
+require 'nordic' .setup {
+    telescope = {
+        -- Available styles: `classic`, `flat`.
+        style = 'flat'
+    },
+    syntax = {
+        comments = {
+            italic = true,
+        },
+        operators = {
+            bold = true
+        },
+        keywords = {
+            bold = true
+        }
+    }
+}
+
+-- Load the scheme.
+vim.cmd [[colorscheme nordic]]

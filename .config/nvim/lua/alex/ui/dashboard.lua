dff691f012bf6e89f60c3579bc2cd7084c52cf4a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-29 23:51:01 +0200

    Neovim: Port to new dashboard.nvim

--- .config/nvim/lua/alex/ui/dashboard.lua
+++ .config/nvim/lua/alex/ui/dashboard.lua
@@ -1,41 +1,53 @@
-local db = require 'dashboard'
+local function pad_front(section, count)
+    for _=1,count do
+        table.insert(section, 1, '')
+    end
+    return section
+end
 
-db.preview_file_height = 11
-db.preview_file_width = 70
-db.custom_center = {
+local function pad_end(section, count)
+    for _=1,count do
+        table.insert(section, '')
+    end
+    return section
+end
+
+local config = {}
+
+config.mru = {}
+config.mru.limit = 10
+
+config.project = {}
+config.project.limit = 5
+
+config.shortcut = {
     {
-        icon = "  ",
-		desc = 'New file      ',
-		action = "enew",
+        desc = '  New file',
+        action = 'enew',
+        group = '@string',
+        key = 'n'
     },
     {
-        icon = '  ',
-        desc = 'Recent files  ',
-        action =  'Telescope oldfiles',
+        desc = '  Update ',
+        action = 'PackerSync',
+        group = '@string',
+        key = 'u'
     },
     {
-        icon = '  ',
-        desc = 'Find file/path',
+        desc = '  File/path ',
         action = 'Telescope find_files find_command=rg,--hidden,--files',
+        group = '@string',
+        key = 'f'
     },
     {
-        icon = '  ',
-        desc = 'Find word     ',
-        action = 'Telescope live_grep',
-    },
-    {
-		icon = "  ",
-		desc = "Update plugins",
-		action = "PackerSync",
-	},
-    {
-        icon = "  ",
-        desc = "Quit          ",
-        action = "q!"
+        desc = '  Quit ',
+        action = 'q!',
+        group = '@macro',
+        key = 'q'
     }
 }
 
-db.custom_header = {
+config.header = {
     '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
     '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
     '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
@@ -44,25 +56,36 @@ db.custom_header = {
     '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
 }
 
-db.custom_footer = {
+config.footer = {
     'ﯦ  Dala what you must.'
 }
 
 -- Default sizes.
 local header_height = 6
-local center_height = 10
+local center_height = config.mru.limit + config.project.limit + 7
 local footer_height = 1
 
 -- Extra padding.
-local header_extra_padding = 1
+local header_extra_padding = 0
 local center_extra_padding = 0
 local footer_extra_padding = 0
 
 -- Get window height in rows.
-local win_height = vim.fn.winheight('%')
+local win_height = vim.api.nvim_win_get_height(0)
 local padding = (win_height - header_height - center_height - footer_height) / 4
 
 -- Calculate and set padding for each section.
-db.header_pad = padding - header_extra_padding
-db.center_pad = padding - center_extra_padding
-db.footer_pad = padding - footer_extra_padding
+local header_pad = padding - header_extra_padding
+local center_pad = padding - center_extra_padding
+local footer_pad = padding - footer_extra_padding
+
+-- Now pad the elements.
+config.header = pad_front(config.header, header_pad)
+config.header = pad_end(config.header, header_pad)
+config.footer = pad_front(config.footer, footer_pad)
+
+-- Setup.
+require 'dashboard' .setup {
+    theme = 'hyper',
+    config = config
+}

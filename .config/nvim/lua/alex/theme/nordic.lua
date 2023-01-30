6f687ce03e607a79520715cb6f3e769f7ea00a95
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-30 12:19:29 +0200

    Neovim: nordic.nvim stuff

--- .config/nvim/lua/alex/theme/nordic.lua
+++ .config/nvim/lua/alex/theme/nordic.lua
@@ -1,19 +1,21 @@
--- Setup.
 require 'nordic' .setup {
-    telescope = {
-        -- Available styles: `classic`, `flat`.
-        style = 'flat'
-    },
-    syntax = {
-        comments = {
-            italic = true,
-        },
-        operators = {
-            bold = true
-        },
-        keywords = {
-            bold = true
-        }
+	telescope = {
+		-- Available styles: `classic`, `flat`.
+		style = 'flat',
+	},
+	-- Enable bold keywords and operators.
+	bold_keywords = true,
+	-- Enable italicized comments.
+	italic_comments = false,
+	-- Enable general editor background transparency.
+	transparent_bg = false,
+	-- Override styling of any highlight group.
+    -- (see next section for an example)
+	override = {},
+    cursorline = {
+        bold = false,
+        -- Avialable styles: 'dark', 'light'.
+        theme = 'light'
     }
 }
 

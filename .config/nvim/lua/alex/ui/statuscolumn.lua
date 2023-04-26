a6816916e219d59a1dc0fde5e20046b957e5e19e
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-04-26 17:22:22 +0200

    Neovim: Check version before configuring statuscolumn (#14)

--- .config/nvim/lua/alex/ui/statuscolumn.lua
+++ .config/nvim/lua/alex/ui/statuscolumn.lua
@@ -1,3 +1,8 @@
+-- Statusclumn was added in 0.9.
+if vim.version.major == 0 and vim.version.minor < 9 then
+    return
+end
+
 -- No separator.
 vim.opt.numberwidth = 4
 vim.opt.statuscolumn = "%= %{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum) : ''}%=%s"

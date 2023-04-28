2ecb20f34650d0c62cc5a4409fea21003103db33
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-04-28 09:43:53 +0200

    Minor

--- .scripts/startup/startup.sh
+++ .scripts/startup/startup.sh
@@ -8,7 +8,8 @@ nvidia-force-comp-pipeline
 picom -b
 
 # Setup the arandr monitor layout AFTER compositor and BEFORE wallpaper.
-~/.screenlayout/default_triple_monitor.sh
+# ~/.screenlayout/default_triple_monitor.sh
+~/.screenlayout/default_double_monitor.sh
 
 # Set wallpaper AFTER compositor.
 feh --bg-fill ~/.wallpapers/Cloud_2_Nord.png

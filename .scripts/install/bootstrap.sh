9a8c0cd57bc5ad0c47352d7a1d377ef2cb4f8bfd
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-05 16:46:31 +0200

    Scripts: More corrections

--- .scripts/install/bootstrap.sh
+++ .scripts/install/bootstrap.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 # Clone.
-pacman -S git
-alias config="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+sudo pacman -S git
 mkdir ~/.dotfiles
-config clone --bare https://github.com/AlexvZyl/.dotfiles ~/.dotfiles/
-config checkout -f
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME clone --bare https://github.com/AlexvZyl/.dotfiles ~/.dotfiles/
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config checkout -f
 
 # Install yay and packages.
 ~/.scripts/packages/setup.sh
 
 # Setup git.
 gh auth login
-config config --local status.showUntrackedFiles no
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 # Get modules.
-config submodule update --recursive --remote
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME submodule update --recursive --remote
 
 # Run main install script.
 ~/.scripts/install/install.sh

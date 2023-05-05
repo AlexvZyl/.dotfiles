3793977762812177b8d7e8e72abcc229a279c814
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-05 18:04:40 +0200

    Scripts: Manually install yay

--- .scripts/packages/bootstrap.sh
+++ .scripts/packages/bootstrap.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Install yay.
+rm -rdf ~/.modules/yay/
+git clone https://aur.archlinux.org/yay.git ~/.modules/yay/
 cd ~/.modules/yay/ || exit
 makepkg -si
 cd ~ || exit
 
-# Update system.
-yay -Syyu
-
-# Install packages.
-yay -S reflector rsync
+# Update and install.
+yay -Syyu reflector rsync
 ~/.scripts/packages/update_mirrorlist.sh
 ~/.scripts/packages/install.sh

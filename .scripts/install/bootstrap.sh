929f8661d4b7e08e1cec3165d32bef918428f8b7
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-05 16:31:44 +0200

    Scripts: Correct order

--- .scripts/install/bootstrap.sh
+++ .scripts/install/bootstrap.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# FIRST!  Install yay and packages.
+# Clone.
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+mkdir ~/.dotfiles
+config clone --bare https://github.com/AlexvZyl/.dotfiles ~/.dotfiles/
+config checkout -f
+
+# Install yay and packages.
 ~/.scripts/packages/setup.sh
 
 # Setup git.
 gh auth login
 config config --local status.showUntrackedFiles no
 
-# Clone.
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-mkdir ~/.dotfiles
-config clone --bare https://github.com/AlexvZyl/.dotfiles ~/.dotfiles/
-config checkout -f
+# Get modules.
+config submodule update --recursive --remote
 
 # Run main install script.
 ~/.scripts/install/install.sh

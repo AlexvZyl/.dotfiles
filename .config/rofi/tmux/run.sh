55cd8d2019083e9fe8d8c51488d0db72c0660201
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-07 12:54:47 +0200

    Rofi: Optimise tmux script

--- .config/rofi/tmux/run.sh
+++ .config/rofi/tmux/run.sh
@@ -1,54 +1,51 @@
 #!/usr/bin/env bash
-source ~/.config/tmux/utils/env.sh
 
-# Define the directory to search for files
+# Setup.
+source ~/.config/tmux/utils/env.sh
 dir="$HOME/.config/tmux/sessions"
 prefix="󱫋  "
 active=" (active)"
 
-# Get list of active sessions.
-active_sessions=()
-while IFS= read -r session; do
-  active_sessions+=("$prefix${session}${active}")
-done < <(~/.config/tmux/utils/workspace_sessions.sh)
-
-# Get a list of files in the directory and store it in an array
-files=("$dir"/*)
-
-# Extract only the filenames from the full paths
-# and store them in a separate array
-filenames=()
-for file in "${files[@]}"; do
-  filename=$(basename "$file")
-  session_name="${filename%.sh}"
-  if tmux has-session -t $session_name 2>/dev/null; then
-    state_name="$session_name$active"
-  else
-    state_name=$session_name
-  fi
-  filenames+=("$prefix${state_name}")
+# Active sessions.
+readarray -t active_sessions < <(~/.config/tmux/utils/workspace_sessions.sh)
+for ((i=0; i<${#active_sessions[@]}; i++)); do
+  active_sessions[i]="$prefix${active_sessions[i]}$active"
 done
 
-all_sessions=("${active_sessions[@]}")
-all_sessions+=("${filenames[@]}")
+# Get configs.
+files=("$dir"/*.sh)
+for (( i=0; i<${#files[@]}; i++ )); do
+  file="${files[i]}"
+  file="${file##*/}"
+  session_name="${file%.sh}"
+  if tmux has-session -t "$session_name" >/dev/null 2>&1; then
+    state_name="$session_name$active"
+  else
+    state_name="$session_name"
+  fi
+  files[i]="${prefix}${state_name}"
+done
 
-# Use Rofi to display the list of filenames as a plain text menu
-selected_file=$(
-    printf '%s\n' "${all_sessions[@]}" | sort -u | \
+# Combine sessions.
+all_sessions=("${active_sessions[@]}")
+all_sessions+=("${files[@]}")
+
+# Display with rofi.
+selected_file=$(printf '%s\n' "${all_sessions[@]}" | sort -u | \
     rofi \
         -config "$HOME/.config/rofi/tmux/style.rasi" \
         -dmenu \
         -p "󱓞  Tmux" \
-        -no-custom
-)
+        -no-custom)
 
-# Check if the user selected a file
+# Start selected session.
 if [[ -n $selected_file ]]; then
     script="${selected_file#$prefix}"
     session_name="${script%$active}"
-    if tmux has-session -t $session_name; then
-        kitty --class tmux-$session_name --execute tmux attach -t $session_name
+    if tmux has-session -t "$session_name" >/dev/null 2>&1; then
+        kitty --class "tmux-$session_name" --execute tmux attach -t "$session_name"
     else
-        kitty --class tmux-$session_name --execute $HOME/.config/tmux/sessions/$session_name.sh
+        kitty --class "tmux-$session_name" --execute "$HOME/.config/tmux/sessions/$session_name.sh"
     fi
 fi
+

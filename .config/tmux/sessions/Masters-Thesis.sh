c3feea6b162bb2588db005a44227018e57aa1c57
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-09 12:20:59 +0200

    Tmux: Correct sessions

--- .config/tmux/sessions/Masters-Thesis.sh
+++ .config/tmux/sessions/Masters-Thesis.sh
@@ -5,7 +5,8 @@ session="Masters-Thesis"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
     path="$HOME/Repositories/Masters-Thesis"
-    tmux new-session -d -s $session -c $path -n nvim "nvim $path"
+    file="$path/TODO.md"
+    tmux new-session -d -s $session -c $path -n nvim "nvim $file"
     tmux new-window -c $path -n "git" "lazygit"
     tmux new-window -c $path -n "shell"
     tmux select-window -t 1
--- .config/tmux/sessions/nvim-nordic.sh
+++ .config/tmux/sessions/nvim-nordic.sh
@@ -4,8 +4,9 @@ source ~/.config/tmux/utils/env.sh
 session="nvim-nordic"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
-    path="$HOME/.local/share/nvim/site/pack/packer/start/nordic.nvim/lua/nordic/init.lua"
-    tmux new-session -d -s $session -c $path -n nvim "nvim $path"
+    path="$HOME/.local/share/nvim/site/pack/packer/start/nordic.nvim/lua/nordic"
+    file="$path/init.lua"
+    tmux new-session -d -s $session -c $path -n nvim "nvim $file"
     tmux new-window -c $path -n "git" "lazygit"
     tmux new-window -c $path -n "shell"
     tmux select-window -t 1

b1882a35187e622b583405058a18d82e85210701
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-09 11:33:11 +0200

    Tmux: Modify sessions

--- .config/tmux/sessions/Masters-Thesis.sh
+++ .config/tmux/sessions/Masters-Thesis.sh
@@ -5,11 +5,9 @@ session="Masters-Thesis"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
     path="$HOME/Repositories/Masters-Thesis"
-    file="$path/USthesis_Masters.tex"
-
-    tmux new-session -d -s $session -c $path -n nvim "nvim $file"
-    tmux new-window -c $path -n "shell"
+    tmux new-session -d -s $session -c $path -n nvim "nvim $path"
     tmux new-window -c $path -n "git" "lazygit"
+    tmux new-window -c $path -n "shell"
     tmux select-window -t 1
 fi
 
--- .config/tmux/sessions/nvim-nordic.sh
+++ .config/tmux/sessions/nvim-nordic.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 source ~/.config/tmux/utils/env.sh
-session="Rust-freelist"
+session="nvim-nordic"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
-    path="$HOME/Repositories/freelist/src"
+    path="$HOME/.local/share/nvim/site/pack/packer/start/nordic.nvim/lua/nordic/init.lua"
     tmux new-session -d -s $session -c $path -n nvim "nvim $path"
-    tmux new-window -c $path -n "shell"
     tmux new-window -c $path -n "git" "lazygit"
+    tmux new-window -c $path -n "shell"
     tmux select-window -t 1
 fi
 

926b727af206bd221a21f835cc3adcdeeb8f4d64
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-24 10:38:12 +0200

    Fish: Update components

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,11 @@
 set fish_greeting "" 
 
 # Tide.
-set -g tide_left_prompt_items context pwd aws docker git newline character
-set -g tide_right_prompt_items jobs cmd_duration status time
-set -g tide_right_prompt_items jobs status os
+# set -g tide_left_prompt_items context pwd aws docker git newline character
+set -g tide_left_prompt_items pwd jobs status newline character
+set -g tide_right_prompt_items docker context os
 set -g tide_git_icon ''
-set -g tide_jobs_icon ' '
+set -g tide_jobs_icon ''
 set -g tide_os_icon ' '
 set -g tide_status_icon_failure ''
 set -g tide_character_icon ""
@@ -15,6 +15,8 @@ set -g tide_pwd_icon_home "  "
 set -g tide_time_format " %H:%M:%S"
 set -g tide_time_bg_color D8DEE9
 set -g tide_os_bg_color D8DEE9
+set -g tide_context_always_display true
+set -g tide_context_color_default EBCB8B
 
 # Pfetch.
 set -gx PF_INFO "ascii title kernel os wm pkgs memory uptime"

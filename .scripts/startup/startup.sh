e0fdd244b4f9ec6e9e3d7ea2be0c4e8349049fb2
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-25 08:40:18 +0200

    Tmux: Start servers on statup

--- .scripts/startup/startup.sh
+++ .scripts/startup/startup.sh
@@ -4,7 +4,8 @@
 (
     nvidia-force-comp-pipeline
     ~/.screenlayout/default_triple_monitor.sh
-    feh --bg-fill ~/.wallpapers/Cloud_2_Nord.png &
+    # feh --bg-fill ~/.wallpapers/Cloud_2_Nord.png &
+    feh --bg-center --bg-fill ~/.wallpapers/Space_Spiral_Nord.png &
     (
         picom -b 
         xborders -c ~/.config/picom/xborder.json
@@ -15,6 +16,7 @@
 # Services
 ~/.config/cron/update_loadshedding.sh &
 dbus-launch dunst --config ~/.config/dunst/dunstrc &
+~/.config/tmux/utils/start_all_servers.sh &
 
 # Remap capslock to escape
 setxkbmap -option caps:escape

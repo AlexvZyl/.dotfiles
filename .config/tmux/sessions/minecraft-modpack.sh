c8d160ebab149c5a600803da0d66bb6e63c4dcac
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-02 22:21:17 +0200

    Tmux: Add mc mod session

---
+++ .config/tmux/sessions/minecraft-modpack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source ~/.config/tmux/utils/workspace_env.sh
+session="minecraft-modpack"
+
+if ! tmux has-session -t $session >/dev/null 2>&1; then
+    path="$HOME/Repositories/minecraft-modpack"
+    file="$path/README.md"
+    tmux new-session -d -s $session -c $path -n nvim "nvim $file"
+    tmux new-window -c $path -n "git" "lazygit"
+    tmux new-window -c "$path/modpack/packwiz" -n "packwiz"
+    tmux new-window -c $path -n "server"
+    tmux select-window -t 1
+fi
+
+tmux attach-session -t $session

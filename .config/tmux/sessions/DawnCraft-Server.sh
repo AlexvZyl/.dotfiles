921dead51bd6ea156b197f43c867b9b9b42563a3
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-07 14:39:43 +0200

    Tmux: ssh with key

--- .config/tmux/sessions/DawnCraft-Server.sh
+++ .config/tmux/sessions/DawnCraft-Server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 source ~/.config/tmux/utils/workspace_env.sh
-session="minecraft-server"
+session="DawnCraft-Server"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
     path="$HOME/Repositories/minecraft-server"
     file="$path/.github/README.md"
     tmux new-session -d -s $session -c $path -n nvim "nvim $file"
     tmux new-window -c $path -n "git" "lazygit"
-    tmux new-window -c $path -n "remote" "ssh mc-server@160.119.253.57"
-    tmux split-window -h "ssh mc-server@160.119.253.57"
+    tmux new-window -c $path -n "remote" "ssh $session"
+    tmux split-window -h "ssh $session"
 fi
 
 tmux attach-session -t $session

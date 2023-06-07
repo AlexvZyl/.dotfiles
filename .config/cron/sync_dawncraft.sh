81da805667cf0273d642096fe2c49aa48a71fde6
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-07 15:31:00 +0200

    Cron: Sync world as well

--- .config/cron/sync_dawncraft.sh
+++ .config/cron/sync_dawncraft.sh
@@ -4,6 +4,11 @@ CUR_PATH=$(dirname "$0")
 source "$CUR_PATH/env.sh"
 
 notify-send "󰚩  DawnCraft Server" "Starting sync..."
-RESULT=$(rsync -avz -e ssh DawnCraft-Server:/home/mc-server/minecraft-server/data/simplebackups/ $HOME/DawnCraft/)
-notify-send "󰚩  DawnCraft Server" "Finished sync."
+
+RESULT=$(rsync -avz -e ssh DawnCraft-Server:/home/mc-server/minecraft-server/data/simplebackups/ $HOME/DawnCraft/simplebackups)
 notify-send --urgency=debug "DawnCraft Server" "$RESULT"
+RESULT=$(rsync -avz -e ssh DawnCraft-Server:/home/mc-server/minecraft-server/data/world/ $HOME/DawnCraft/world)
+notify-send --urgency=debug "DawnCraft Server" "$RESULT"
+
+notify-send "󰚩  DawnCraft Server" "Finished sync."
+

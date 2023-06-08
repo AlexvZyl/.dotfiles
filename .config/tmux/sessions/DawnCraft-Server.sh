023c6acd50ae537d6e08ea779b5b9fa192d9203a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-08 15:40:19 +0200

    Tmux: Improve server session script

--- .config/tmux/sessions/DawnCraft-Server.sh
+++ .config/tmux/sessions/DawnCraft-Server.sh
@@ -8,8 +8,14 @@ if ! tmux has-session -t $session >/dev/null 2>&1; then
     file="$path/.github/README.md"
     tmux new-session -d -s $session -c $path -n nvim "nvim $file"
     tmux new-window -c $path -n "git" "lazygit"
-    tmux new-window -c $path -n "remote" "ssh $session"
-    tmux split-window -h "ssh $session"
+    tmux new-window -n "remote-monitor" "ssh $session"
+    tmux send-keys "btop" Enter
+    tmux new-window -n "remote-logs" "ssh $session"
+    tmux send-keys "cd minecraft-server" Enter
+    tmux send-keys "nvim-logs" Enter
+    tmux new-window -n "remote-shell" "ssh $session"
+    tmux send-keys "cd minecraft-server" Enter
+    tmux send-keys "clear" Enter
 fi
 
 tmux attach-session -t $session

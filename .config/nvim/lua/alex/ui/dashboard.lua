6634dfeac7623c8322867e1da3ace6dd430ce0a8
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-07-13 17:31:16 +0200

    Neovim: Cleanup dashboard

--- .config/nvim/lua/alex/ui/dashboard.lua
+++ .config/nvim/lua/alex/ui/dashboard.lua
@@ -1,9 +1,7 @@
-local hyper = true
-
 local config = {}
 
 config.mru = {}
-config.mru.limit = 5
+config.mru.limit = 10
 
 config.project = {}
 config.project.limit = 10
@@ -46,14 +44,6 @@ config.shortcut = {
 
 config.week_header = {}
 config.week_header.enable = true
--- config.header = {
--- '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
--- '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
--- '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
--- '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
--- '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
--- '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
--- }
 
 config.footer = {
     '',
@@ -63,72 +53,7 @@ config.footer = {
 config.packages = {}
 config.packages.enable = true
 
--- Default sizes.
-local header_height = 10
-local center_height = config.mru.limit + config.project.limit + 3
-local footer_height = 1
-
--- Get window height in rows.
-local win_height = vim.api.nvim_win_get_height(0)
-local padding = (win_height - header_height - center_height - footer_height) / 4
-
--- Now pad the elements.
-config.packages.bottom_padding = 3
-config.packages.top_padding = 1
-config.header_bottom_padding = 1
-config.footer_top_padding = 3
-
-if hyper then
-    require('dashboard').setup {
-        theme = 'hyper',
-        config = config,
-    }
-    return
-end
-
-local custom_center = {
-    {
-        icon = '  ',
-        desc = 'New file      ',
-        action = 'enew',
-    },
-    {
-        icon = '  ',
-        desc = 'Recent files  ',
-        action = 'Telescope oldfiles',
-    },
-    {
-        icon = '  ',
-        desc = 'Find file/path',
-        action = 'Telescope find_files find_command=rg,--hidden,--files',
-    },
-    {
-        icon = '  ',
-        desc = 'Find word     ',
-        action = 'Telescope live_grep',
-    },
-    {
-        icon = '  ',
-        desc = 'Update plugins',
-        action = 'PackerSync',
-    },
-    {
-        icon = '  ',
-        desc = 'Quit          ',
-        action = 'q!',
-    },
-}
-
 require('dashboard').setup {
-    theme = 'doom',
-    config = {
-        week_header = {
-            enable = true,
-        },
-        header_bottom_padding = 3,
-        footer_top_padding = 3,
-        header = config.header,
-        center = custom_center,
-        footer = config.footer,
-    },
+    theme = 'hyper',
+    config = config,
 }

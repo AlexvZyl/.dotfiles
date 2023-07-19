dd203d25ad75ed5e7713ea4c2f4d01155722423b
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-07-19 11:18:05 +0200

    Tmux: Correct nvim session

--- .config/tmux/sessions/nvim-nordic.sh
+++ .config/tmux/sessions/nvim-nordic.sh
@@ -4,7 +4,7 @@ source ~/.config/tmux/utils/workspace_env.sh
 session="nvim-nordic"
 
 if ! tmux has-session -t $session >/dev/null 2>&1; then
-    path="$HOME/.local/share/nvim/site/pack/packer/start/nordic.nvim/lua/nordic"
+    path="$HOME/.local/share/nvim/lazy/nordic.nvim/lua/nordic"
     file="$path/init.lua"
     tmux new-session -d -s $session -c $path -n nvim "nvim $file"
     tmux new-window -c $path -n "git" "lazygit"

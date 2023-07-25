cc7fea7dddad129856a617476389470651bfb1c7
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-07-25 14:05:03 +0200

    Fish: Fix

--- .scripts/install/shell.sh
+++ .scripts/install/shell.sh
@@ -2,7 +2,7 @@
 
 fish <<'END_FISH'
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-    fisher install IlanCosman/tide@v5t
+    fisher install IlanCosman/tide@v5
     echo "3\
           2\
           2\

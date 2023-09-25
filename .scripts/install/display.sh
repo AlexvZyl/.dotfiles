2240c15594be0cba3442a5ff9b480a87c57de37a
AlexvZyl <alexandervanzyl@protonmail.com>
2023-09-25 21:15:03 +0200

    Scripts: Fix dm not using correct wallpaper

--- .scripts/install/display.sh
+++ .scripts/install/display.sh
@@ -5,8 +5,7 @@ source "$(dirname $0)/../utils.sh"
 # SDDM Login Manager
 sudo systemctl disable display-manager
 sudo systemctl enable sddm
-sudo cp $USER_HOME/.wallpapers/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/
-sudo mv /usr/share/sddm/themes/sugar-candy/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/wall_secondary.png
+sudo cp $USER_HOME/.wallpapers/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountains.jpg
 
 # Setup lock screen.
 # Should this script run every time the screens change?  Yeah.

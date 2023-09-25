8b0d6725259adbfcd88f1d4e619a9d084591ca99
Alexander van Zyl <calexandervanzyl@gmail.com>
2023-09-25 13:02:53 +0200

    Scripts: Add copy of utils for bootstrap

--- .scripts/install/bootstrap.sh
+++ .scripts/install/bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-source "$(dirname $0)/../utils.sh"
+if [ -z ${SUDO_USER} ]
+then
+    export USER_HOME="$HOME"
+else
+    export USER_HOME="/home/${SUDO_USER}"
+fi
 
 # Clone.
 sudo pacman -S git

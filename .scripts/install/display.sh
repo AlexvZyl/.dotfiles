49ad669f7ed96b0abad3db4b6052d2acd2cd47f0
AlexvZyl <alexandervanzyl@protonmail.com>
2023-09-26 17:03:13 +0200

    Display: Tokyonight lockscreen and login screen

--- .scripts/install/display.sh
+++ .scripts/install/display.sh
@@ -5,9 +5,20 @@ source "$(dirname $0)/../utils.sh"
 # SDDM Login Manager
 sudo systemctl disable display-manager
 sudo systemctl enable sddm
-sudo cp $USER_HOME/.wallpapers/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountains.jpg
+
+# Nordic
+# sudo cp $USER_HOME/.wallpapers/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountains.jpg
+
+# Tokyonight
+sudo cp $USER_HOME/.wallpapers/Tokyonight_Street_1.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountains.jpg
 
 # Setup lock screen.
 # Should this script run every time the screens change?  Yeah.
-betterlockscreen -u $USER_HOME/.wallpapers/National_Park_Nord.png --display 1
-betterlockscreen -u $USER_HOME/.wallpapers/National_Park_Nord.png --blur 0.5 --display 1
+
+# Nordic
+# betterlockscreen -u $USER_HOME/.wallpapers/National_Park_Nord.png --display 1
+# betterlockscreen -u $USER_HOME/.wallpapers/National_Park_Nord.png --blur 0.5 --display 1
+
+# Tokyonight
+betterlockscreen -u $USER_HOME/.wallpapers/Tokyonight_Street_1.png --display 1
+betterlockscreen -u $USER_HOME/.wallpapers/Tokyonight_Street_1.png --blur 0.5 --display 1

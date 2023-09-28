a25c93167b4a0c0a87e55aeacef04f40d1145b00
AlexvZyl <alexandervanzyl@protonmail.com>
2023-09-28 12:19:29 +0200

    More tweaks

--- .scripts/install/display.sh
+++ .scripts/install/display.sh
@@ -10,7 +10,7 @@ sudo systemctl enable sddm
 # sudo cp $USER_HOME/.wallpapers/National_Park_Nord.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountain.jpg
 
 # Tokyonight
-sudo cp $USER_HOME/.wallpapers/Tokyonight_Street_1.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountain.jpg
+sudo cp $USER_HOME/.wallpapers/Tokyonight_Street_2.png /usr/share/sddm/themes/sugar-candy/Backgrounds/Mountain.jpg
 
 # Setup lock screen.
 # Should this script run every time the screens change?  Yeah.

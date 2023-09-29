a21c3f0a8995685894e67d2a8659421250951c76
AlexvZyl <alexandervanzyl@protonmail.com>
2023-09-29 12:02:49 +0200

    Scripts: Start with security and shellcheck

--- .scripts/install/install.sh
+++ .scripts/install/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-source "$(dirname $0)/../utils.sh"
+source "$(dirname "$0")/../utils.sh"
 
-$USER_HOME/.scripts/install/dual-boot.sh
-$USER_HOME/.scripts/install/links.sh
-$USER_HOME/.scripts/install/display.sh
-$USER_HOME/.scripts/install/hardware.sh
-$USER_HOME/.scripts/install/misc.sh
-$USER_HOME/.scripts/install/security.sh
+"$USER_HOME/.scripts/install/security.sh"
+"$USER_HOME/.scripts/install/dual-boot.sh"
+"$USER_HOME/.scripts/install/links.sh"
+"$USER_HOME/.scripts/install/display.sh"
+"$USER_HOME/.scripts/install/hardware.sh"
+"$USER_HOME/.scripts/install/misc.sh"
 
-
-$USER_HOME/.tmux/plugins/tpm/tpm
-$USER_HOME/.tmux/plugins/tpm/bin/install_plugins
+"$USER_HOME/.tmux/plugins/tpm/tpm"
+"$USER_HOME/.tmux/plugins/tpm/bin/install_plugins"

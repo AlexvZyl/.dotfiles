163bdb39050081f72ac6b753cdcc2065288e120b
AlexvZyl <alexandervanzyl@protonmail.com>
2023-10-03 15:03:14 +0200

    Polybar: Fix loadshedding region

--- .config/polybar/scripts/update_loadshedding.py
+++ .config/polybar/scripts/update_loadshedding.py
@@ -2,13 +2,22 @@
 import subprocess
 import json
 import os
+"""
+Get location:
+
+```
+command = [ "curl", "--location", "--request", "GET",
+            'https://developer.sepush.co.za/business/2.0/areas_nearby?lat=-33.932106&lon=18.860151',
+            "--header", f"token: {key}" ]
+```
+"""
 
 key = os.environ.get("ESKOM_SE_PUSH_KEY")
-region = "westerncape-2-universityofstellenbosch"
+region = "eskme-2-stellenboschstellenboschwesterncape"
 
-command = [ "curl", "--location", "--request", "GET",  
-            "https://developer.sepush.co.za/business/2.0/area?id="+region,
-            "--header", f"token: {key}" ]
+command = ["curl", "--location", "--request", "GET",
+           "https://developer.sepush.co.za/business/2.0/area?id="+region,
+           "--header", f"token: {key}"]
 
 try:
     response = subprocess.run(command, stdout=subprocess.PIPE, stderr = subprocess.DEVNULL).stdout.decode('utf-8')

e1b4773135eb3c467720458de800935b8d84bf6e
AlexvZyl <alexandervanzyl@protonmail.com>
2023-10-05 18:49:10 +0200

    Feat: Add polkit and lockscreen generation to startup

--- .scripts/startup/startup.sh
+++ .scripts/startup/startup.sh
@@ -14,7 +14,11 @@
 ~/.config/cron/update_loadshedding.sh &
 ~/.config/tmux/utils/start_all_servers.sh &
 dbus-launch dunst --config ~/.config/dunst/dunstrc &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 # Keyboard stuff
 setxkbmap -option caps:escape
 xset r rate 165 50
+
+# Regenerate lock screen
+nice -n 20 betterlockscreen -u ~/.wallpapers/tokyo-night-space_upscaled.png --display 1 &

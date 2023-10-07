941080e45fc64b7663bdb1e80e79bee7c33da360
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-10-07 13:45:54 +0200

    Scripts: Run security last

--- .scripts/install/install.sh
+++ .scripts/install/install.sh
@@ -2,7 +2,6 @@
 
 source "$(dirname "$0")/../utils.sh"
 
-"$USER_HOME/.scripts/install/security.sh"
 "$USER_HOME/.scripts/install/dual-boot.sh"
 "$USER_HOME/.scripts/install/links.sh"
 "$USER_HOME/.scripts/install/display.sh"
@@ -11,3 +10,5 @@ source "$(dirname "$0")/../utils.sh"
 
 "$USER_HOME/.tmux/plugins/tpm/tpm"
 "$USER_HOME/.tmux/plugins/tpm/bin/install_plugins"
+
+"$USER_HOME/.scripts/install/security.sh"

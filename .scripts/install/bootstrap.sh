2f92269dcaab21329b273e8b361485139602838e
AlexvZyl <alexandervanzyl@protonmail.com>
2023-10-07 15:12:54 +0200

    Install: Manually update submodules (for now)

--- .scripts/install/bootstrap.sh
+++ .scripts/install/bootstrap.sh
@@ -12,6 +12,7 @@ sudo pacman -S git
 mkdir ~/.dotfiles
 git --git-dir=$USER_HOME/.dotfiles/ --work-tree=$USER_HOME clone --bare https://github.com/AlexvZyl/.dotfiles $USER_HOME/.dotfiles/
 git --git-dir=$USER_HOME/.dotfiles/ --work-tree=$USER_HOME checkout -f
+source $USER_HOME/.profile
 
 # Needed for install.
 $USER_HOME/.scripts/install/links.sh
@@ -21,10 +22,13 @@ $USER_HOME/.scripts/packages/bootstrap.sh
 
 # Setup git.
 gh auth login
-git --git-dir=$USER_HOME/.dotfiles/ --work-tree=$USER_HOME config --local status.showUntrackedFiles no
+config config --local status.showUntrackedFiles no
 
 # Get modules.
-git --git-dir=$USER_HOME/.dotfiles/ --work-tree=$USER_HOME submodule update --recursive --init
+config submodule update --init --force --remote .password-store/
+config submodule update --init --force --remote .private/
+config submodule update --init --force --remote .modules/user.js/
+config submodule update --init --force --remote .config/nvim/
 
 # Run main install script.
 $USER_HOME/.scripts/install/install.sh

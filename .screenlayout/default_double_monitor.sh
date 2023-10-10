36dd1a70cdb5dbaead813b23f14b8d7b9c0ea30b
AlexvZyl <alexandervanzyl@protonmail.com>
2023-10-10 17:05:12 +0200

    Screenlayout: Update default double

--- .screenlayout/default_double_monitor.sh
+++ .screenlayout/default_double_monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x496 --rotate normal
+xrandr --output DP-0 --primary --rate 75 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 1920x496 --rotate normal --rate 144

9385cbd06575fdc39889ee6e0518f44a1d7922fa
AlexvZyl <alexandervanzyl@protonmail.com>
2024-04-05 12:50:05 +0200

    Ref: Remove files and scripts not longer used

--- .scripts/install/links.sh
+++ .scripts/install/links.sh
@@ -2,9 +2,6 @@
 
 source "$(dirname $0)/../utils.sh"
 
-sudo ln -f -s "$USER_HOME/.etc/pacman.conf" "/etc/pacman.conf"
-sudo ln -f -s "$USER_HOME/.etc/sddm.conf" "/etc/sddm.conf"
-
 sudo rm -rdf "$USER_HOME/.ssh"
 sudo ln -f -s -d "$USER_HOME/.private/.ssh/" "$USER_HOME/"
 

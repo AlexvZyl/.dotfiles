593c82160d131d2c4112dbff75ec616f9167598a
AlexvZyl <alexandervanzyl@protonmail.com>
2024-04-06 13:15:25 +0200

    Ref(tmux): Switch to workspace if it already exists

--- .config/tmux/utils/create_workspace.sh
+++ .config/tmux/utils/create_workspace.sh
@@ -6,6 +6,14 @@ if [ -n "$1" ]; then
 else
     WORKSPACE_NAME=$(basename "$(pwd)")
 fi
+
+
+if tmux has-session -t "$WORKSPACE_NAME" >/dev/null 2>&1; then
+    tmux switch -t "$WORKSPACE_NAME"
+    exit 0
+fi
+
+
 tmux rename-session "$WORKSPACE_NAME"
 
 CURRENT_WINDOW=$(tmux display-message -p '#I')

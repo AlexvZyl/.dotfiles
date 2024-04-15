b7e03500e5061f543049fe03ad0ceb5ee760df48
AlexvZyl <alexandervanzyl@protonmail.com>
2024-04-15 17:57:59 +0200

    Nix: Add LSPs

--- .config/nixos/lsp.nix
+++ .config/nixos/lsp.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }:
 
 {
-
+  environment.systemPackages = with pkgs; [
+    rust-analyzer
+    python311Packages.flake8
+    lua-language-server
+    pyright
+    stylua
+    luajitPackages.luacheck
+    pylint
+    shellcheck
+    vscode-extensions.ms-vscode.cpptools
+    terraform-ls
+    gopls
+  ];
 }
+

76b07b0d5c54d31cd97e7d61ca5470347ea12c0a
AlexvZyl <alexandervanzyl@protonmail.com>
2024-04-24 10:21:36 +0200

    Nix: Improve network config

--- .config/nixos/network.nix
+++ .config/nixos/network.nix
@@ -6,13 +6,9 @@
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.allowedUDPPorts = [ 80 443 ];
-  networking.firewall.enable = false;
-
-  networking.resolvconf.dnsExtensionMechanism = true;
-  services.resolved.enable = true;
+  networking.firewall.enable = true;
 
   services.fail2ban.enable = false;
   services.fail2ban.maxretry = 5;
   services.fail2ban.bantime = "-1";
 }
-

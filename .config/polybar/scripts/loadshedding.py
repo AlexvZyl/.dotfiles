251b773d50dd6ba82c03e61f79bba9cc50b03b45
AlexvZyl <alexandervanzyl@protonmail.com>
2024-05-27 12:34:37 +0200

    Polybar: Tweak icons

--- .config/polybar/scripts/loadshedding.py
+++ .config/polybar/scripts/loadshedding.py
@@ -31,12 +31,12 @@ def find_next_event(events):
         time_left = end - now
         hours, remainder = divmod(int(time_left.total_seconds()), 3600)
         minutes, _ = divmod(remainder, 60)
-        print("%{F#db4b4b} %{F#e0af68}󱎫 %{F-}" + f"{hours:02d}:{minutes:02d}" + " %{F#db4b4b}")
+        print("%{F#db4b4b} %{F#e0af68}󱎫 %{F-}" + f"{hours:02d}:{minutes:02d}" + " %{F#} ")
 
     # Display next loadshedding.
     else:
         duration = end - start
-        print("%{F#e0af68}%{F-} " + start.strftime("%H:%M") + " %{F#e0af68}󱎫 %{F-}" + str(int(duration.total_seconds()/3600)) + "h")
+        print("%{F#e0af68} %{F-}" + start.strftime("%H:%M") + " %{F#e0af68}󱎫 %{F-}" + str(int(duration.total_seconds()/3600)) + "h")
 
 
 def main():
@@ -48,7 +48,7 @@ def main():
             events = json.loads(json.load(file))["events"]
 
             if no_loadshedding(events):
-                print("%{F#9ece6a}%{F-} None %{F#9ece6a}%{F-}")
+                print("%{F#9ece6a} %{F-}None %{F#9ece6a} %{F-}")
                 return
 
             find_next_event(events)

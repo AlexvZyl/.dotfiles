3789728bc1c0f9590e4d0f6cc7eda62b23f55f75
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-04 09:17:49 +0200

    Nix: Add docker and /bin/bash

--- .nixos/user.nix
+++ .nixos/user.nix
@@ -77,4 +77,13 @@
       ];
     }
   ];
+
+  system.activationScripts.binbash = {
+    deps = [ "binsh" ];
+    text = ''
+         ln -s /bin/sh /bin/bash
+    '';
+  };
+
+  virtualisation.docker.enable = true;
 }

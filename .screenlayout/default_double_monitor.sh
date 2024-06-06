08315d31cc75e218e27f02978babe72fe0b47fec
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-06 22:39:54 +0200

    Update monitors

--- .screenlayout/default_double_monitor.sh
+++ .screenlayout/default_double_monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 1920x392 --rotate normal --output VIRTUAL1 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x420 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output USB-C-0 --mode 1920x1080 --pos 1920x0 --rotate right

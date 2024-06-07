06be275357acd4089142096c5360a6fc627db39e
AlexvZyl <alexandervanzyl@protonmail.com>
2024-06-07 19:52:58 +0200

    Updates

--- .nixos/gpu.nix
+++ .nixos/gpu.nix
@@ -1,15 +1,17 @@
 { config, ... }:
 
 {
-  services.xserver.videoDrivers = ["intel" "nvidia"];
+  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia" "intel"];
 
   hardware.nvidia = {
     modesetting.enable = true;
-    open = true;
+    open = false;
     nvidiaSettings = true;
     forceFullCompositionPipeline = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     powerManagement.enable = true;
+    nvidiaPersistenced = true;
   };
 
   hardware.opengl = {

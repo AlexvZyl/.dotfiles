93487bb5151b96ea227857c0fe209e14b2566306
AlexvZyl <alexandervanzyl@protonmail.com>
2024-09-25 16:11:34 +0200

    Remove old polybar

--- .config/polybar.old/scripts/rss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-OUTPUT=$(newsboat -x reload print-unread)
-
-if echo "$OUTPUT" | grep -q "Error"; then
-    :
-else
-    echo "$OUTPUT" | grep -o '[0-9]*'
-fi
--- .config/polybar.old/scripts/update_loadshedding.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-source ~/.private/env.sh || exit
-python ~/.config/polybar/scripts/update_loadshe

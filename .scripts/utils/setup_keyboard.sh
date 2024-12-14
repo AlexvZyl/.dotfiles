73250ab326283d443c85950830be61575a03e61c
AlexvZyl <alexandervanzyl@protonmail.com>
2024-12-14 11:37:14 +0200

    Updates

--- .scripts/utils/setup_keyboard.sh
+++ .scripts/utils/setup_keyboard.sh
@@ -4,4 +4,4 @@ setxkbmap -option caps:escape
 xmodmap -e 'keycode 112 = NoSymbol'
 xmodmap -e 'keycode 117 = NoSymbol'
 
-xset r rate 165 50
+xset r rate 165 55
--- .scripts/utils/start_playback.sh
+++ .scripts/utils/start_playback.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pactl load-module module-loopback latency_msec=1000
-
+pactl load-module module-loopback latency_msec=0

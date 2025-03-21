09c521a34a69b8f0e840c08250ba665ad1cfbcb0
AlexvZyl <alexandervanzyl@protonmail.com>
2025-03-21 10:19:41 +0200

    Update scripts

--- .config/polybar/scripts/tf2_servers.py
+++ .config/polybar/scripts/tf2_servers.py
@@ -1,41 +1,44 @@
 from time import sleep
 import a2s
-from tf2_za_server_list import servers
 
-# Constants.
+# Config.
 MIN_PLAYERS = 0
-SUCCESS = False
 TIMEOUT = 1
 RETRIES = 5
 
+SERVERS = [
+    ["102.182.203.60", 27015],
+    ["129.151.169.190", 27017],
+    ["129.151.169.190", 27015],
+]
+
+
 # Get the number of players on the server.
-def status(server):
+def get_server_status(server):
     for _ in range(RETRIES):
         try:
-            server = a2s.info((server[0], server[1]), timeout = TIMEOUT)
-            global SUCCESS
-            SUCCESS = True
-            return server
-        except (TimeoutError, OSError):
+            return a2s.info((server[0], server[1]), timeout=TIMEOUT)
+        except (TimeoutError):
             sleep(0.1)
     return False
 
-# Find optimal server.
-max = 0
-max_cap = 0
-name = ""
-for server in servers:
-    s = status(server)
-    if s and s.player_count > MIN_PLAYERS:
-        max = s.player_count
-        max_cap = s.max_players
-        name = s.server_name
 
-# Print status.
-if SUCCESS:
+def Main():
+    # Find optimal server.
+    max = 0
+    max_cap = 0
+    for server in SERVERS:
+        status = get_server_status(server)
+        if status and status.player_count > MIN_PLAYERS:
+            print(status.player_count)
+            max = status.player_count
+            max_cap = status.max_players
+
+    # Print status.
     if max == 0 or max < MIN_PLAYERS:
-        print("󰒎  Offline")
+        print("󰒎 ")
     else:
-        print(f"󰒍  {name}: {max:02}/{max_cap:02}")
-else:
-    print("  Error")
+        print(f"󰒍 {max:02}/{max_cap:02}")
+
+
+Main()

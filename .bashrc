b39bce69083e92396b00237e598458342025a31d
AlexvZyl <alexandervanzyl@protonmail.com>
2025-09-06 11:31:06 +0200

    Updates

--- .bashrc
+++ .bashrc
@@ -1,9 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#shellcheck disable=1090,1091
 
-source "$HOME/.profile"
-if [ -e ~/.private/env.sh ]; then
-    source ~/.private/env.sh
-fi
 
-zoxide init bash | source
-[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+Main() {
+    source "$HOME/.profile"
+    
+    local env_file="$HOME/.private/env.sh"
+    if [[ -f $env_file ]]; then
+        source "$env_file"
+    fi
+    
+    # TODO: Why does this complain?
+    zoxide init bash | source 2>/dev/null
+    [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+}
+
+
+Main "$@"
--- .config/tmux/utils/create_workspace.sh
+++ .config/tmux/utils/create_workspace.sh
@@ -1,29 +1,34 @@
-#!/bin/sh
+#!/bin/bash -e
 
 
-# Get name.
-if [ -n "$1" ]; then
-    WORKSPACE_NAME=$1
-else
-    WORKSPACE_NAME=$(basename "$(pwd)")
-fi
+Main() {
+    # Get name.
+    if [ -n "$1" ]; then
+        WORKSPACE_NAME=$1
+    else
+        WORKSPACE_NAME=$(basename "$(pwd)")
+    fi
 
 
-# Switch to existing session if it already exists.
-if tmux has-session -t "$WORKSPACE_NAME" >/dev/null 2>&1; then
-    tmux switch -t "$WORKSPACE_NAME"
-    exit 0
-fi
+    # Switch to existing session if it already exists.
+    if tmux has-session -t "$WORKSPACE_NAME" >/dev/null 2>&1; then
+        tmux switch -t "$WORKSPACE_NAME"
+        exit 0
+    fi
 
 
-# Create new session.
-tmux rename-session "$WORKSPACE_NAME"
-CURRENT_WINDOW=$(tmux display-message -p '#I')
-tmux rename-window "editor"
-tmux new-window -c "./" -n "git" "lazygit"
-tmux new-window -c "./" -n "shell" "fish"
-tmux select-window -t "$CURRENT_WINDOW"
-tmux swap-window -t +1
-tmux next-window
-tmux select-window -t 1
-nvim .
+    # Create new session.
+    tmux rename-session "$WORKSPACE_NAME"
+    CURRENT_WINDOW=$(tmux display-message -p '#I')
+    tmux rename-window "editor"
+    tmux new-window -c "./" -n "git" "lazygit"
+    tmux new-window -c "./" -n "shell" "fish"
+    tmux select-window -t "$CURRENT_WINDOW"
+    tmux swap-window -t +1
+    tmux next-window
+    tmux select-window -t 1
+    nvim .
+}
+
+
+Main "$@"
